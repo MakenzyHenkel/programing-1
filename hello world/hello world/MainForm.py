98270f5f7fea26d8b64b55d0a87971d585914038
MakenzyHenkel <henkel.m@stu.janesville.k12.wi.us>
2024-09-24 09:35:08 -0500

    Initial commit

---
+++ hello world/hello world/MainForm.py
@@ -0,0 +1,80 @@
+﻿import System.Drawing
+import System.Windows.Forms
+
+from System.Drawing import *
+from System.Windows.Forms import *
+
+class MainForm(Form):
+    def __init__(self):
+        self.InitializeComponent()
+    
+    def InitializeComponent(self):
+        self._label1 = System.Windows.Forms.Label()
+        self._button1 = System.Windows.Forms.Button()
+        self._button2 = System.Windows.Forms.Button()
+        self._button3 = System.Windows.Forms.Button()
+        self.SuspendLayout()
+        # 
+        # label1
+        # 
+        self._label1.BackColor = System.Drawing.SystemColors.ButtonFace
+        self._label1.Font = System.Drawing.Font("Microsoft Sans Serif", 18, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._label1.Location = System.Drawing.Point(98, 63)
+        self._label1.Name = "label1"
+        self._label1.Size = System.Drawing.Size(694, 163)
+        self._label1.TabIndex = 0
+        self._label1.Text = """
+"""
+        self._label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        # 
+        # button1
+        # 
+        self._button1.Location = System.Drawing.Point(170, 245)
+        self._button1.Name = "button1"
+        self._button1.Size = System.Drawing.Size(75, 23)
+        self._button1.TabIndex = 1
+        self._button1.Text = "show"
+        self._button1.UseVisualStyleBackColor = True
+        self._button1.Click += self.Button1Click
+        # 
+        # button2
+        # 
+        self._button2.Location = System.Drawing.Point(406, 245)
+        self._button2.Name = "button2"
+        self._button2.Size = System.Drawing.Size(75, 23)
+        self._button2.TabIndex = 2
+        self._button2.Text = "clear"
+        self._button2.UseVisualStyleBackColor = True
+        self._button2.Click += self.Button2Click
+        # 
+        # button3
+        # 
+        self._button3.Location = System.Drawing.Point(655, 245)
+        self._button3.Name = "button3"
+        self._button3.Size = System.Drawing.Size(75, 23)
+        self._button3.TabIndex = 3
+        self._button3.Text = "exit"
+        self._button3.UseVisualStyleBackColor = True
+        self._button3.Click += self.Button3Click
+        # 
+        # MainForm
+        # 
+        self.BackColor = System.Drawing.SystemColors.ActiveCaption
+        self.ClientSize = System.Drawing.Size(901, 338)
+        self.Controls.Add(self._button3)
+        self.Controls.Add(self._button2)
+        self.Controls.Add(self._button1)
+        self.Controls.Add(self._label1)
+        self.Name = "MainForm"
+        self.Text = "hello world"
+        self.ResumeLayout(False)
+
+
+    def Button1Click(self, sender, e):
+        self._label1.Text="hello world"
+
+    def Button2Click(self, sender, e):
+        self._label1.Text=""
+
+    def Button3Click(self, sender, e):
+        Application.Exit()
